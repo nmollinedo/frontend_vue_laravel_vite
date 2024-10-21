3b08103d87c39071cb239a4bb199d1b8a377001d
boris.mita <borisleonel12@gmail.com>
2024-10-21 09:15:41 -0400

    fix: cambio puerto

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3002
+    port: 80
   },
   optimizeDeps: {
     noDiscovery: true
